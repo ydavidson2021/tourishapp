78255f75f7fa88b636985ea378a0ab7afed58bc6
Yenny D <ydavidson2021@gmail.com>
2020-11-09 11:59:27 -0600

    Icons, Favorites, and Comments

--- components/SiteInfoComponent.js
+++ components/SiteInfoComponent.js
@@ -1,32 +1,71 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Text, View, ScrollView, FlatList } from 'react-native';
+import { Card, Icon } from 'react-native-elements';
 import { SITES } from '../shared/sites';
+import { COMMENTS } from '../shared/comments';
 
-function RenderSite({site}) {
-
+function RenderSite(props) {
+    const {site} = props;
     if (site) {
         return (
             <Card
                 featuredTitle={site.name}
-                image={require('./images/britmuseum.jpg')}
-            >
+                image={require('./images/britmuseum.jpg')}>
                 <Text style={{margin: 10}}>
                     {site.description}
                 </Text>
+                <Icon
+                    name={props.favorite ? 'heart' : 'heart-o'}
+                    type='font-awesome'
+                    color='#f50'
+                    raised
+                    reverse
+                    onPress={() => props.favorite ? 
+                        console.log('Already set as a favorite') : props.markFavorite()}
+                />
             </Card>
-        );
-    }
-    return <View />;
+        ); 
+    } 
+    return <View />; 
 }
 
-class SiteInfo extends Component {
+function RenderComments({comments}) {
 
+    const renderCommentItem = ({item}) => {
+        return (
+            <View style={{margin: 10}}>
+                <Text style={{fontSize: 14}}>{item.text}</Text>
+                <Text style={{fontSize: 12}}>{item.rating} Stars</Text>
+                <Text style={{fontSize: 12}}>{`-- ${item.author}, ${item.date}`}</Text>
+            </View>
+        );
+    };
+
+    return (
+        <Card title='Comments'>
+            <FlatList
+                data={comments}
+                renderItem={renderCommentItem}
+                keyExtractor={item => item.id.toString()}
+            />
+        </Card>
+    );
+}
+
+
+class SiteInfo extends Component {
+    
     constructor(props) {
         super(props);
         this.state = {
-            sites: SITES
+            sites: SITES, 
+            comments: COMMENTS, 
+            favorite: false
         };
+    }
+
+    markFavorite() {
+        this.setState({favorite: true});
     }
 
     static navigationOptions = {
@@ -36,7 +75,16 @@ class SiteInfo extends Component {
     render() {
         const siteId = this.props.navigation.getParam('siteId');
         const site = this.state.sites.filter(site => site.id === siteId)[0];
-        return <RenderSite site={site} />;
+        const comments = this.state.comments.filter(comment => comment.siteId === siteId);
+        return (
+            <ScrollView>
+                <RenderSite site={site}
+                    favorite={this.state.favorite}
+                    markFavorite={() => this.markFavorite()}
+                />
+                <RenderComments comments={comments} />
+            </ScrollView>
+        );
     }
 }
 
---
+++ shared/comments.js
@@ -0,0 +1,67 @@
+export const COMMENTS = 
+[
+    {
+        id: 0,
+        siteId: 0,
+        rating: 5, 
+        text: "What a magnificent view!",
+        author: "Joe Smith",
+        date: "2020-10-25T16:30Z"
+    },
+    {
+        id: 1,
+        siteId: 0,
+        rating: 4, 
+        text: "The museum was beautiful, I will definitely go there in person after the pandemic is over.",
+        author: "Jane Smith",
+        date: "2020-06-17T03:33Z"
+    },
+    {
+        id: 2,
+        siteId: 0,
+        rating: 5, 
+        text: "Just wowza",
+        author: "Chelsea F",
+        date: "2020-02-18T18:12Z"
+    },
+    {
+        id: 3,
+        siteId: 0,
+        rating: 5, 
+        text: "Gorgeous",
+        author: "Madonna",
+        date: "2020-08-04T20:11Z"
+    },
+    {
+        id: 4,
+        siteId: 0,
+        rating: 4, 
+        text: "What a great view",
+        author: "Sally Wiggles",
+        date: "2020-07-23T19:44Z"
+    },
+    {
+        id: 5,
+        siteId: 1,
+        rating: 4, 
+        text: "I don't need to travel and I get to see this beautiful museum - win! win!",
+        author: "Julia R",
+        date: "2020-06-17T03:33Z"
+    },
+    {
+        id: 6,
+        siteId: 1,
+        rating: 5, 
+        text: "Elegant",
+        author: "Taylor M. ",
+        date: "2020-02-18T18:12Z"
+    },
+    {
+        id: 7,
+        siteId: 1,
+        rating: 5, 
+        text: "I don't need a passport and ride a plane. This is wonderful. Thanks for bringing this experience to me.",
+        author: "Lindsay B. ",
+        date: "2020-08-04T20:11Z"
+    }
+]
